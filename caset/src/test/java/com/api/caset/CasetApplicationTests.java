cb16ecbbf4f214cffe98f8b36aaf41f65c8fb358
fency <fencyj854@gmail.com>
2024-11-23 11:39:03 +0530

    Initial commit

---
+++ caset/src/test/java/com/api/caset/CasetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.caset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
